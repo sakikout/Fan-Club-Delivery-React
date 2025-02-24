d6019cae2112be548df2181dfb746df12bd66148
sakikout <bedalfior@gmail.com>
2025-02-24 15:23:03 -0300

    Filling out the SettingsPage
    We're so back

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useUser } from "../components/context/UserProvider"
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, ListGroup, Row, Col, Card } from 'react-bootstrap';
+import { FaPen, FaKey, FaCreditCard, FaUserSlash } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import CustomNavBar from '../components/NavBar';
 
 
 const SettingsPage = () => {
-  const navigate = useNavigate();
   const { user } = useUser();
 
     return (
       <>
       <CustomNavBar></CustomNavBar>
       <Container className="gap-2 mt-3 mb-3">
+        <p className="fw-bold fs-4">Configurações</p>
 
+      <ListGroup className="gap-2 mt-3 mb-3">
+        <ListGroup.Item action><FaPen></FaPen> Alterar Nome</ListGroup.Item>
+        <ListGroup.Item action><MdEmail></MdEmail> Alterar E-mail</ListGroup.Item>
+        <ListGroup.Item action><FaKey></FaKey> Alterar Senha</ListGroup.Item>
+        <ListGroup.Item action><FaCreditCard></FaCreditCard> Gerenciar Cartões no Aplicativo</ListGroup.Item>
+        <ListGroup.Item action variant="danger">
+          <FaUserSlash></FaUserSlash> Deletar Conta
+        </ListGroup.Item>
+      </ListGroup>
+
+      <Card>
+        <Card.Body>
+        <Card.Title>titulo</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">corpo</Card.Subtitle>
+        <Card.Text>
+        </Card.Text>
+        </Card.Body>
+      </Card>
         
       </Container>
        </>
