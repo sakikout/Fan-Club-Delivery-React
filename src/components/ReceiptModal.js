3cdb3e59f44a3ac8a572ab2437b8f5628244ccf6
sakikout <bedalfior@gmail.com>
2025-03-11 14:41:40 -0300

    Removed toFixed
    not working again... yeah...

--- src/components/ReceiptModal.js
+++ src/components/ReceiptModal.js
@@ -18,16 +18,13 @@ const ReceiptModal = (props) => {
       const fetchCards = async () => {
         if (user) {
           const userOrder = await firestoreService.getOrderById(id);
-          console.log(userOrder);
           setOrderInfo(userOrder);
         }
       };
   
       fetchCards();
   
-      console.log(orderInfo);
-  
-    }, []);
+    }, [id, user]);
 
     const formatDate = (isoString) => {
         return format(new Date(isoString), "dd/MM/yyyy - HH:mm", { locale: ptBR });
@@ -68,7 +65,7 @@ const ReceiptModal = (props) => {
             <p className="mb-1">Endereço: {orderInfo.order.address} </p>
             <p className="mb-1">Forma de Pagamento: {orderInfo.order.paymentMethod}</p>
             {orderInfo.order.paymentMethod === "Dinheiro" && <p>Troco para: R$ {orderInfo.order.changeFor}</p>}
-            <h5 className="mt-3">Total: R$ {orderInfo.order.total.toFixed(2)}</h5>
+            <h5 className="mt-3">Total: R$ {orderInfo.order.total}</h5>
             <p className="text-muted">Entrega estimada para: {formatDate(orderInfo.estimatedDeliveryTime)} min</p>
             <p>Status: <span className="fw-bold">{orderInfo.status}</span></p>
             </>
